db85fd429e43ff3d17e3e7b2b60be8eef89857d7
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-16 18:18:09 +0600

    add content and also add some style

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,18 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='w-screen h-screen'>
-      <div></div>
+    <div className='w-9/12 h-[80vh] bg-gray-500 shadow-lg flex items-center justify-around'>
+      <div>
+        <Image src="/logo.svg" alt="me" width="50" height="50" />
+        <h1>Welcome to <br /> DiGiLABS</h1>
+        <form>
+          <input type="text" name="username" id="" />
+          <input type="password" name="password" id="" />
+          <input type="submit" value="Next" />
+        </form>
+        <p>Forgot your password?</p>
+        <p>Not member? <span>Create account</span></p>
+      </div>
       <div>
         <Image src="/auth.svg" alt="me" width="500" height="500" />
       </div>
