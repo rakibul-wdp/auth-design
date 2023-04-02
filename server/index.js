d95fb7460f9709424e6e94ff03b3f8c9d11747af
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-02 11:54:52 +0600

    create boilarplate of index file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log("hello");
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
