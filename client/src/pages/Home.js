e82fd7150a8a283f2a7cbb4c98739b6b31913521
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-02 16:09:13 +0600

    add cors and change api in client

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('https://full-stack-auth-server.onrender.com/api/workouts', {
+      const response = await fetch('/api/workouts', {
         headers: { 'Authorization': `Bearer ${user.token}` },
       })
       const json = await response.json()
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
 
@@ -10,6 +11,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
