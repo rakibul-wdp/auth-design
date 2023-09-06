d021aaf24eca6a7361f3d22c958a73ce46ad67f8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-06 08:28:54 +0600

    create token of jwt

---
+++ just-auth/server/controllers/userController.js
@@ -0,0 +1,6 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
