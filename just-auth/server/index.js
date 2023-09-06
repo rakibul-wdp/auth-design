7003c3efcc1b10e1dd8cec6f4afd71f1f8c63c6a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-06 07:58:34 +0600

    setup index file and connect with mongodb

---
+++ just-auth/server/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+// app.use("/api/user", "hello user");
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
