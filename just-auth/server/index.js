4b2ab4671fe2c974eea329c27c37619148e41b9e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-06 09:36:55 +0600

    create client site with react vite

--- just-auth/server/index.js
+++ just-auth/server/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const userRoutes = require("./routes/user");
 
 // express app
 const app = express();
@@ -17,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 // routes
-// app.use("/api/user", "hello user");
+app.use("/api/user", userRoutes);
 
 // connect to db
 mongoose
