7055104e08a004602033bc7646d871828e22a10e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-06 08:34:37 +0600

    create signup controllers

--- just-auth/server/controllers/userController.js
+++ just-auth/server/controllers/userController.js
@@ -20,3 +20,21 @@ const loginUser = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+// signup a user
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { signupUser, loginUser };
